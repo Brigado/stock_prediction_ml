8cf10ac14a1cb2476bf044dbb7a144ca2682f62b
Chantal <chantal.pellegrini@gmail.com>
2018-10-18 11:16:23 +0800

    new emotion mapping relative, data analysis

---
+++ data_analysis.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.utils import shuffle
+import numpy as np
+
+def pos_neg_neu(x):
+    if x == 'positive':
+        return 1
+    elif x == 'negative':
+        return -1
+    else:
+        return 0
+
+data = pd.read_csv('news_to_emotions_relative.csv')
+
+#split data
+data_0 = data[data['is_volatile'] == 0]
+data_1 = data[data['is_volatile'] == 1]
+
+#find correlations
+features = ['anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'trust', 'sentiment_dict', 'sentiment_lib_avg' ]
+print("volatility:     0         |       1")
+for feature in features:
+    neg = data_0[feature].mean()
+    pos = data_1[feature].mean()
+    print("{}: {} | {}".format(feature, neg, pos))
--- news_to_emotions.py
+++ news_to_emotions.py
@@ -7,7 +7,7 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
 ps = PorterStemmer()
 
 emotions = ['anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness', 'surprise', 'trust', 'positive', 'negative']
-col = ['market_date', 'company_symbol', 'sentiment', 'anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness','surprise', 'trust', 'is_volatile']
+col = ['market_date', 'company_symbol', 'anger', 'anticipation', 'disgust', 'fear', 'joy', 'sadness','surprise', 'trust', 'sentiment_dict', 'sentiment_lib_avg', 'is_volatile']
 emotion_dfs = {}
 dict_path_dir = "data/dictionaries/"
 for dict in os.listdir(dict_path_dir):
@@ -48,21 +48,34 @@ for date in dateList:
     if (i%500 == 0):
         print(i)
         df_result = pd.DataFrame(lst, columns=col)
-        df_result.to_csv("news_to_emotions{}.csv".format(i))
+        df_result.to_csv("news_to_emotions_relative{}.csv".format(i))
 
     for company in companyList:
         df_date_company = df.loc[(df['market_date'] == date) & (df['company_symbol'] == company), 'headline'].values
         # create bag of stemmed words out of headlines
         wordset = set()
+        sentiments = []
         for hl in df_date_company:
+            #compute sentiment with library:
+            sia = SIA()
+            pol_score = sia.polarity_scores(hl)
+            sentiments.append(pol_score['compound'])
+
+            #dictionary preparation
             words = word_tokenize(hl)
             for word in words:
                 wordset.add(ps.stem(word))
         emotions, sentiment_str = get_emotions(wordset)
+        if sentiment_str == "positive":
+            sent = 1
+        elif sentiment_str == "negative":
+            sent = -1
+        else:
+            sent = 0
 
         if len(wordset) > 0:
             volatility = df.loc[(df['market_date'] == date) & (df['company_symbol'] == company), 'is_volatile'].values[0]
-            lst.append([date, company, emotions['anger'], emotions['anticipation'], emotions['disgust'], emotions['fear'], emotions['joy'], emotions['sadness'], emotions['surprise'], emotions['trust'], sentiment_str, volatility])
+            lst.append([date, company, float(emotions['anger'])/float(len(wordset)), float(emotions['anticipation'])/float(len(wordset)), float(emotions['disgust'])/float(len(wordset)), float(emotions['fear'])/float(len(wordset)), float(emotions['joy'])/float(len(wordset)), float(emotions['sadness'])/float(len(wordset)), float(emotions['surprise'])/float(len(wordset)), float(emotions['trust'])/float(len(wordset)), sent, sum(sentiments)/float(len(sentiments)), volatility])
 
 df_result = pd.DataFrame(lst, columns=col)
-df_result.to_csv("news_to_emotions.csv")
+df_result.to_csv("news_to_emotions_relative.csv")
